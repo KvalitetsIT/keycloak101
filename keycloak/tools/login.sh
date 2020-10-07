6e1f31e0e6f6fddde0c196772c02061eb8aaa828
evtr <eva@kvalitetsit.dk>
2020-10-07 16:16:59 +0200

    create user

--- keycloak/tools/login.sh
+++ keycloak/tools/login.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 curl -d "client_id=admin-cli" -d "username=sd" -d "password=Test1234" -d "grant_type=password" http://keycloak:8080/auth/realms/master/protocol/openid-connect/token | jq -r '.access_token' 
-
-export KUK=blabla
